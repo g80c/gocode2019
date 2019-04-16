8ea56c28ece69e17a5c18882fabd9ec3f2cfc64d
Ryan Holway <ryanholway@Ryans-MacBook-Pro-6.local>
2019-04-16 16:59:24 -0600

    adding graphs

--- README.md
+++ README.md
@@ -4,24 +4,40 @@ The first dataset we focused on was 'Degrees Awarded to Post-Secondary Graduates
 
 Description of the dataset: 'Demographics for all certificate, degree, or formal award approved by Colorado Department of Higher Education (CDHE) for students since 2001. Demographics include data on age, ethnicity, program name, and residency.'
 
-## Breakdown of Institutions attended
+## Breakdown of Institutions' Awardees
 
-There are 29 unique institutions described in the dataset. For any given year, if the attendees associated with the institution was greater than 5% of total attendees for the year, the institution is represented. For all institutions which make up less than or equal to 5% of total attendees, that institution is grouped into 'Other'
+There are 29 unique institutions described in the dataset. For any given year, if the awardees associated with the institution was greater than 5% of total awardees for the year, the institution is represented. For all institutions which make up less than or equal to 5% of total awardees, that institution is grouped into 'Other'
 
 ![2001](images/institution_piecharts/2001.png)  | ![2009](images/institution_piecharts/2009.png)  | ![2017](images/institution_piecharts/2017.png)
 ------------- | -------------  | -------------
 
-From 2001 to 2017 we can see still about half of institution attendees are enrolling in smaller institutions (those individual institutions which make of less than 5% of total attendees for the year)
+From 2001 to 2017 we can see still about half of institution awardees are enrolling in smaller institutions (those individual institutions which make of less than 5% of total awardees for the year).
 
-We see the percent of attendees enrolling in community colleges and other more affordable institutions increasing. Front Range Community Collage, Colorado Mountain College, and Pikes Peak Community College are all more prevalent in 2017 than they were in 2001.
+We see the percent of awardees enrolling in community colleges and other more affordable institutions increasing. Front Range Community College, Colorado Mountain College, and Pikes Peak Community College are all more prevalent in 2017 than they were in 2001.
 
-On the flip-side, larger more expensive institutions have seen the percent of attendees decreasing. This can be seen with Colorado State University, University of Colorado Denver, and University of Colorado Boulder from 2001 to 2017.
+On the flip-side, larger more expensive institutions have seen the percent of awardees decreasing. This can be seen with Colorado State University, University of Colorado Denver, and University of Colorado Boulder from 2001 to 2017.
 
-## Breakdown of Degrees Pursued
+## Breakdown of Awarded Degrees
 
-Attendees of the institutions are in various degree programs, which include: Associate, Bachelor, Certificate, Doctoral, Masters, OtherGraduate, Professional, Specialist. We can look at how the breakdown of degrees have changed from 2001 to 2017.
+Awardees of the institutions are in various degree programs, which include: Associate, Bachelor, Certificate, Doctoral, Masters, OtherGraduate, Professional, Specialist. We can look at how the breakdown of degrees have changed from 2001 to 2017.
 
 ![2001](images/degree_piecharts/2001.png)  | ![2009](images/degree_piecharts/2009.png)  | ![2017](images/degree_piecharts/2017.png)
 ------------- | -------------  | -------------
 
-The percent of attendees enrolling in Bachelor and Masters degree programs have taken a dip from 2001 and 2017, while the percent of those attending institutions for Certificate degrees has increased. This seems to support the 'Breakdown of Institutions' analysis - less percentages of people are opting to go with the more expensive education options (large universities and longer degree programs) and more are opting to go with the less expensive options (more affordable universities and shorter degree programs). 
+The percent of awardees enrolling in Bachelor and Masters degree programs have taken a dip from 2001 and 2017, while the percent of those awardees from institutions for Certificate degrees has increased (almost doubled). This seems to support the 'Breakdown of Institutions' analysis - less percentages of people are opting to go with the more expensive education options (large universities and longer degree programs) and more are opting to go with the less expensive options (more affordable universities and shorter degree programs).
+
+## Breakdown of Awardees Programs
+
+Awardees of the institutions are more specifically in certain programs. The programs vary from sociology to welding to law and everything in between. Over 500 specific programs are detailed. We categorized these programs into nine buckets to make for easier analysis. The buckets are:
+* Computer Science
+* Art/History
+* Social Science
+* Math/Engineering
+* Physical/Medical/Wellness
+* Law
+* General Science
+* Labor
+* Business
+
+![2001](images/program_piecharts/2001.png)  | ![2009](images/program_piecharts/2009.png)  | ![2017](images/program_piecharts/2017.png)
+------------- | -------------  | -------------
--- src/eda1.py
+++ src/eda1.py
@@ -4,18 +4,76 @@ import matplotlib.pyplot as plt
 
 
 def label_uni(row):
-    if row['perc'] > 5.0:
+    '''
+    use this helper function to label smaller universities as 'other'
+    '''
+    if row['perc'] > 3.0:
+        return row['unique_values']
+    else:
+        return 'Other'
+
+def label_uni_start(row):
+    '''
+    use this helper function to label smaller universities as 'other' for the
+    perc change df
+    '''
+    if row['start_perc'] > 2.5:
+        return row['unique_values']
+    else:
+        return 'Other'
+
+def label_uni_end(row):
+    '''
+    use this helper function to label smaller universities as 'other' for the
+    perc change df
+    '''
+    if row['end_perc'] > 3.0:
         return row['unique_values']
     else:
         return 'Other'
 
 def label_degree(row):
+    '''
+    use this helper function to label smaller degrees as 'other'
+    '''
     if row['perc'] > 1.0:
         return row['unique_values']
     else:
         return 'Other'
 
+def label_program(row):
+    '''
+    use this helper function to bucket the many degree programs into about 9
+    individual buckets rather than 31
+    '''
+    if row['cip2'] == 10 or row['cip2'] == 11:
+        return 'Computer Science'
+    elif row['cip2'] == 12 or row['cip2'] == 16 or row['cip2'] == 23 or \
+    row['cip2'] == 24 or row['cip2'] == 50 or row['cip2'] == 54:
+        return 'Arts/History'
+    elif row['cip2'] == 13 or row['cip2'] == 25 or row['cip2'] == 30 or \
+    row['cip2'] == 38 or row['cip2'] == 42 or row['cip2'] == 44 or row['cip2'] == 45:
+        return 'Social Science'
+    elif row['cip2'] == 14 or row['cip2'] == 15 or row['cip2'] == 27 or row['cip2'] == 49:
+        return 'Math/Engineering'
+    elif row['cip2'] == 19 or row['cip2'] == 51 or row['cip2'] == 31:
+        return 'Physical/Medical/Wellness'
+    elif row['cip2'] == 43 or row['cip2'] == 22:
+        return 'Law'
+    elif row['cip2'] == 26 or row['cip2'] == 40 or row['cip2'] == 41:
+        return 'General Sciences'
+    elif row['cip2'] == 46 or row['cip2'] == 47 or row['cip2'] == 48:
+        return 'Labor'
+    elif row['cip2'] == 52:
+        return 'Business'
+    else:
+        return 'Other'
+
 def pie_chart_inst_name_by_year(year, title, save_as):
+    '''
+    create a pie chart of the attendees of universities in Colorado as percentages
+    by year
+    '''
     temp_df = df[df['year']==year]
     value_counts = temp_df['institutionName'].value_counts(dropna=True, sort=True)
     temp_df = value_counts.rename_axis('unique_values').reset_index(name='counts')
@@ -29,9 +87,14 @@ def pie_chart_inst_name_by_year(year, title, save_as):
     ax1.axis('equal')
     ax1.set_title(title)
     plt.savefig('../images/institution_piecharts/' + save_as)
-    # print (temp_df)
+
+# def percent_increase_inst
 
 def pie_chart_degree_level_by_year(year, title, save_as):
+    '''
+    create a pie chart of the degrees of students in Colorado as percentages
+    by year
+    '''
     temp_df = df[df['year']==year]
     value_counts = temp_df['degreeLevel'].value_counts(dropna=True, sort=True)
     temp_df = value_counts.rename_axis('unique_values').reset_index(name='counts')
@@ -46,11 +109,42 @@ def pie_chart_degree_level_by_year(year, title, save_as):
     ax1.set_title(title)
     plt.savefig('../images/degree_piecharts/' + save_as)
 
+def pie_chart_program_by_year(year, title, save_as):
+    '''
+    create a pie chart of the programs of students in Colorado as percentages
+    by year
+    '''
+    temp_df = df2[df2['year'] == year]
+    value_counts = temp_df['programBucket'].value_counts(dropna=True, sort=True)
+    temp_df = value_counts.rename_axis('unique_values').reset_index(name='counts')
+    temp_df['perc'] = (temp_df['counts']/temp_df['counts'].sum())*100
+    labels = temp_df['unique_values'].tolist()
+    counts = temp_df['perc'].tolist()
+    colors = ['#fb7d07', '#0203e2', '#d0c101', '#738595', '#e03fd8', '#645403',
+    '#8e82fe', '#e50000', '#019529']
+    fig1, ax1 = plt.subplots()
+    ax1.pie(counts, labels=labels, colors=colors, autopct='%1.1f%%')
+    ax1.axis('equal')
+    ax1.set_title(title)
+    plt.savefig('../images/program_piecharts/' + save_as)
+
+def college_type(x):
+    if isinstance(x, str) and 'community' in x.lower().split():
+        return 'community college'
+    elif isinstance(x, str) and 'university' in x.lower().split():
+        return 'university'
+    elif isinstance(x, float):
+        return x
+    else:
+        return 'state college'
+
 
 
 if __name__ == '__main__':
     df = pd.read_csv('../../data_misc/data/grad_degrees.csv')
-
+    df2 = df.filter(items=['year', 'cip2', 'programName']).dropna(subset = ['programName'])
+    df2['programBucket'] = df2.apply(lambda row: label_program(row), axis=1)
+    df['type'] = df['institutionName'].apply(college_type)
     # create pie charts of institutuion breakdowns
     # pie_chart_inst_name_by_year(2001, '2001 Breakdown of Institutions', '2001')
     # pie_chart_inst_name_by_year(2005, '2005 Breakdown of Institutions', '2005')
@@ -59,6 +153,48 @@ if __name__ == '__main__':
     # pie_chart_inst_name_by_year(2017, '2017 Breakdown of Institutions', '2017')
 
     # create pie charts of degree level breakdowns
-    pie_chart_degree_level_by_year(2001, '2001 Breakdown of Degrees', '2001')
-    pie_chart_degree_level_by_year(2009, '2009 Breakdown of Degrees', '2009')
-    pie_chart_degree_level_by_year(2017, '2017 Breakdown of Degrees', '2017')
+    # pie_chart_degree_level_by_year(2001, '2001 Breakdown of Degrees', '2001')
+    # pie_chart_degree_level_by_year(2009, '2009 Breakdown of Degrees', '2009')
+    # pie_chart_degree_level_by_year(2017, '2017 Breakdown of Degrees', '2017')
+
+    # create pie charts of program level breakdowns
+    # pie_chart_program_by_year(2001, '2001 Breakdown of Programs', '2001')
+    # pie_chart_program_by_year(2005, '2005 Breakdown of Programs', '2005')
+    # pie_chart_program_by_year(2009, '2009 Breakdown of Programs', '2009')
+    # pie_chart_program_by_year(2013, '2013 Breakdown of Programs', '2013')
+    # pie_chart_program_by_year(2017, '2017 Breakdown of Programs', '2017')
+
+    # start_df = df[df['year']==2001]
+    # value_counts = start_df['institutionName'].value_counts(dropna=True, sort=True)
+    # start_df = value_counts.rename_axis('unique_values').reset_index(name='counts')
+    # start_df['start_perc'] = (start_df['counts']/start_df['counts'].sum())*100
+    # start_df['insts'] = start_df.apply(lambda row: label_uni_start(row), axis=1)
+    # start_df = start_df.groupby('insts')['start_perc'].sum().reset_index()
+    #
+    # end_df = df[df['year']==2017]
+    # value_counts = end_df['institutionName'].value_counts(dropna=True, sort=True)
+    # end_df = value_counts.rename_axis('unique_values').reset_index(name='counts')
+    # end_df['end_perc'] = (end_df['counts']/end_df['counts'].sum())*100
+    # end_df['insts'] = end_df.apply(lambda row: label_uni_end(row), axis=1)
+    # end_df = end_df.groupby('insts')['end_perc'].sum().reset_index()
+    #
+    # perc_change_df = start_df.merge(end_df, left_on='insts', right_on='insts', how='outer')
+    # perc_change_df = perc_change_df.dropna(subset = ['start_perc', 'end_perc'])
+    # perc_change_df['%change'] = ((perc_change_df['end_perc'] - perc_change_df['start_perc']) / perc_change_df['start_perc'])*100
+    # perc_change_df = perc_change_df.sort_values(by=['%change'])
+    # perc_change_df['positive'] = perc_change_df['%change'] > 0
+    #
+    # labels = perc_change_df['insts'].tolist()
+    # labels2 = ['CU-Boulder', 'UNC', 'CSU', 'DU', 'MSU-Denver', 'CU-Colorado Springs',
+    # 'Other', 'Front Range CC', 'Pueblo CC', 'Pikes Peak CC', 'Red Rocks CC']
+    # values = perc_change_df['%change'].tolist()
+    # fig, ax = plt.subplots()
+    # index = np.arange(len(labels2))
+    # bar_width = 0.35
+    # plt.bar(index, values, bar_width, color=perc_change_df['positive'].map({True: 'b', False: 'r'}))
+    # plt.xticks(index + bar_width/2, labels2, rotation=85)
+    # plt.tight_layout()
+    # plt.axhline(y=0, color='black')
+    # plt.title('% Change from 2001-2017 of % of Student Awardees')
+    # plt.xlabel('* awardee - student earning a certificate, degree, or formal award. 100% of awardees contains all of Colorado')
+    # plt.show()
